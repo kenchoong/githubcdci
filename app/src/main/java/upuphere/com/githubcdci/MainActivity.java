787aab11a0c07ff920119c9eddf3287f116ee29b
Ken Choong <matchartproduction@gmail.com>
2020-04-12 18:28:47 +0800

    first commit

---
+++ app/src/main/java/upuphere/com/githubcdci/MainActivity.java
@@ -0,0 +1,14 @@
+package upuphere.com.githubcdci;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
