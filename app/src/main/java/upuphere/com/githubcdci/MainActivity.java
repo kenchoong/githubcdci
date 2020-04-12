b25d10501e1b0650bc436e1f75ad08b7c52f9245
Ken Choong <matchartproduction@gmail.com>
2020-04-12 18:35:36 +0800

    abc

--- app/src/main/java/upuphere/com/githubcdci/MainActivity.java
+++ app/src/main/java/upuphere/com/githubcdci/MainActivity.java
@@ -3,6 +3,7 @@ package upuphere.com.githubcdci;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showAbc();
+    }
+
+    private void showAbc(){
+        Toast.makeText(this, "ABC", Toast.LENGTH_SHORT).show();
     }
 }
